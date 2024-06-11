dd4a57df29c0d159886c7f9a4dabd80738184c5a
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-06-11 13:34:55 -0300

    chore: Add newsletter form component to home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { NgOptimizedImage } from '@angular/common';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import { NewsletterFormComponent } from "../newsletter-form/newsletter-form.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
-  imports: [HeaderComponent, NgOptimizedImage, BtnPrimaryComponent],
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    imports: [HeaderComponent, NgOptimizedImage, BtnPrimaryComponent, NewsletterFormComponent]
 })
 export class HomeComponent {}
