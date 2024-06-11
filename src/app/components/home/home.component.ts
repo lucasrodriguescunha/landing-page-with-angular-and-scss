bafe7f55cf8545d970b10f28642cb4296f9b2187
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-06-10 23:09:34 -0300

    chore: Add new files and update dependencies

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+  imports: [HeaderComponent, NgOptimizedImage],
+})
+export class HomeComponent {}
